cf696d2344477f468a2f7405da38a9ce9a38bba3
Brian Snyder <bsnyder74@gmail.com>
2024-09-16 14:53:25 -0700

    update lint action

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  return (
+    <main className="main">
+      <div>
+        <h2>Hello World</h2>
+        <p>new test here</p>
+      </div>
+      <Link href="/about">About</Link>
+      <br />
+    </main>
+  );
+}
